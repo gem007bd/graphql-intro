beda7b47d416abfc37d32188df8048d35cec1e88
gem007bd <gem007bd@yahoo.com>
2019-05-31 12:41:56 +0200

    graphql with apollo server

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const express = require('express');
+const {
+    ApolloServer,
+    gql
+} = require('apollo-server');
+
 const app = express();
 
-app.get('/graphql', (req, res, next) => {
-    res.send('Hello graphql');
+/**
+ * Construct a schema, using GraphQL schema language
+ * The GraphQL schema
+ */
+const typeDefs = gql `
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+/**
+ * Provide resolver functions for your schema fields
+ */
+const resolvers = {
+    Query: {
+        hello: () => 'world'
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
 });
 
-app.listen({port: 4000}, () => {
-    console.log(`Server ready at port 4000`);
+
+server.listen().then(({
+    url
+}) => {
+    console.log(`🚀 Server ready at ${url}`)
 });
